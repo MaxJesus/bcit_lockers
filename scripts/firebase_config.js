0a1ba9d9d0fdb5688e3ba36e00ac957c4251a9c7
Rickson2019 <2528889836@qq.com>
2019-11-22 05:05:00 -0800

    some fixes

---
+++ scripts/firebase_config.js
@@ -0,0 +1,17 @@
+    // the web app's Firebase configuration
+    //"bcit-lockers" is the project name
+    var firebaseConfig = {
+      apiKey: "AIzaSyASFWH5KsgmLDY3HcRUmDIwbFtYv5Ayf8o",
+      authDomain: "bcit-lockers.firebaseapp.com",
+      databaseURL: "https://bcit-lockers.firebaseio.com",
+      projectId: "bcit-lockers",
+      storageBucket: "bcit-lockers.appspot.com",
+      messagingSenderId: "380958555512",
+      appId: "1:380958555512:web:3a1928344614d15da51c8d",
+      measurementId: "G-R8V8WMCJRB"
+    };
+    // Initialize Firebase
+
+    firebase.initializeApp(firebaseConfig);
+//  Do not know how it works yet.
+//  firebase.analytics();
